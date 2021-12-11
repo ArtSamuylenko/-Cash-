378c7c453be87af82b5268aad35ba7b989747ca4
ArtSamuylenko <89385274775@mail.ru>
2021-12-12 01:37:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace dengi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double stoimost;
+
+            stoimost = double.Parse(Console.ReadLine());
+
+
+            if (stoimost < 500)
+            {
+                Console.WriteLine($"Цена товаров с учетом скидки составяляет: {stoimost}");
+
+            }
+            else if (stoimost < 1000 && stoimost >= 500)
+            {
+                Console.WriteLine($"Цена товаров с учетом скидки составяляет: {stoimost * 0.95}");
+            }
+            else
+            {
+                Console.WriteLine($"Цена товаров с учетом скидки составяляет: {stoimost * 0.9}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
